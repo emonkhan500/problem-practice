fe6787459e0905013329c1f01ec2b3d38bf42279
emonkhan500 <khdilo47@gmail.com>
2025-07-26 23:07:08 +0600

    parctise

---
+++ basic.js
@@ -0,0 +1,69 @@
+// array
+//  const array=[ 'emin', 'lol' , 'due']
+//  console.log(array.indexOf('lol'));
+
+//  array[3]= 'new'
+//  array[2]=4
+//  console.log(array);
+
+
+
+
+//  const name= ['emon', 'shakil','azima',5 ,true]
+
+
+//  if(name.includes(5)){
+//     console.log('emon er shate khan add kor');
+//  }
+//  else{
+//     console.log('khan barite nai');
+//  }
+
+
+//  console.log(Array.isArray(name));
+
+
+
+
+// const num=[2,3,6,8]
+// // num.pop(11)
+
+
+// num.shift()
+// num.shift()
+// num.unshift(0)
+// num.unshift(0)
+// console.log(num);
+
+
+// const fruits =['apple','banana','orange','pinaple','grapes']
+
+
+
+// console.log(fruits[3]);
+
+// fruits[2]='emon'
+// console.log(fruits);
+
+// const destination = ['coxs bazar','sundarban','sent-martin']
+
+
+// destination.push('emon')
+// destination.unshift('start')
+// console.log(destination);
+
+// const books= ['bangla','english','javascript','react'];
+
+// if(books.includes('javascript')){
+//     console.log('javascript ase');
+// }
+// else{
+//     console.log('js re nia aso');
+// }
+
+const array1= [1,2,3,4,5];
+const array2= ['emon','shakil','azima']
+
+
+const newArray = array1.concat(array2)
+console.log(newArray);
---
+++ conditional.js
@@ -0,0 +1,40 @@
+// const bill = 500;
+// if(bill>500){
+//     console.log('coke is free');
+// }
+// else{
+//     console.log('coke: 30 tk');
+// }
+
+// const height= 2.5;
+// const weight= 47;
+
+// const BMI =weight / height^2
+
+
+
+// if(BMI >= 24.9 && BMI <29){
+//     console.log('you r okay');
+// }
+// else if(BMI >29){
+//     console.log('you r mota');
+// }
+// else{
+//     console.log(' tmi chikon');
+// }
+
+const num1= 4;
+const num2 = 4;
+let result= 0
+
+// if( num1> num2){
+//     result= num1 *2
+// }
+// else{
+//     result= num1+num2
+// }
+
+
+result= num1>num2 ? num1*2: num1 +num2
+
+console.log(result);
