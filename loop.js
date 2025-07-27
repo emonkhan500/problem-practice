8c0f87a42e4e3d6e4d6c929397e0baff057d19c8
emonkhan500 <khdilo47@gmail.com>
2025-07-27 16:31:19 +0600

    parctise

---
+++ loop.js
@@ -0,0 +1,31 @@
+const friends=['elon','mark','bill','waren','jeph']
+
+
+// for( i=0; i<friends.length; i++){
+//     console.log(friends[i]);
+// }
+
+
+// let i =0
+// while( i<friends.length){
+//     console.log(friends[i]);
+//     i++
+// }
+
+const numbers=[34,56,43,67,234,233,78]
+
+for( const num of numbers){
+    console.log(num);
+}
+
+
+
+// for( i=0; i<numbers.length; i++){
+//     console.log(numbers[i]);
+// }
+
+// let i=0
+// while( i<numbers.length){
+// console.log(numbers[i]);
+// i++
+// }
---
+++ loop2.js
@@ -0,0 +1,35 @@
+// let i=0
+//  while( i<=200){
+//     if(i===100){
+//         break
+//     }
+//     console.log(i);
+//     i++
+//  }
+
+//  closure
+// function outer() {
+//   let count = 0;
+
+//   function inner() {
+//     count++;
+//     console.log(count);
+//   }
+
+//   return inner;
+// }
+
+// const counter = outer(); // outer() runs and returns inner()
+// counter(); // 1
+
+
+
+// 
+const original = [1, 2, 3];
+
+// Using spread operator
+// const copy1 = [...original];
+
+// Using slice()
+const copy2 = original.slice(2);
+console.log(copy2);
